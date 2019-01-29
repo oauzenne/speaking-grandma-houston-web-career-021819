2359c344380f47853855839cf1c2ab820e88abb4
Olivia Auzenne <oauzenne@gmail.com>
2019-01-29 14:34:37 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,25 +1,3 @@
-#def speak_to_grandma(phrase)
- # if phrase.downcase
-  #  puts "HUH?! SPEAK UP, SONNY!"
-  #else if phrase.upcase
-  # puts "NO, NOT SINCE 1938!"
- # else "I LOVE YOU GRANDMA!"
-  #  puts "I LOVE YOU TOO PUMPKIN!"
-#  end
-#end
-
-def speak_to_grandma(phrase)
-  if phrase=="I LOVE YOU GRANDMA!"
-    puts "I LOVE YOU TOO PUMPKIN!"
-  elsif phrase=="Hi Nana, how are you?"
-    puts "HUH?! SPEAK UP, SONNY!
-  elsif phrase=="Hi!"
-    puts "HUH?! SPEAK UP, SONNY!"
-  elsif phrase=="WHAT DID YOU EAT TODAY?"
-    puts "NO, NOT SINCE 1938!"
-  elsif phrase=="WHAT?"
-    puts "NO, NOT SINCE 1938!"
-  end
-end
+def 
 
   
