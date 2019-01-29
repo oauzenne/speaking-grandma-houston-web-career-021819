f6681dc1fd95d48dbced7474cac15842fb287018
Olivia Auzenne <oauzenne@gmail.com>
2019-01-29 14:35:17 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,4 +1,11 @@
 def speak_to_grandma
-  
+  if say == "I LOVE YOU GRANDMA!"
+    puts "I LOVE YOU TOO PUMPKIN!"
+    elsif say == say.upcase
+        "NO, NOT SINCE 1938!"
+    else
+      "HUH?! SPEAK UP, SONNY!"
+  end
+end
 
   
