147caeb19c5502e64f3bd2ab3e334779d847e30a
Olivia Auzenne <oauzenne@gmail.com>
2019-01-29 14:35:52 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,10 +1,10 @@
-def speak_to_grandma
-  if say == "I LOVE YOU GRANDMA!"
+def speak_to_grandma(phrase)
+  if phrase == "I LOVE YOU GRANDMA!"
     puts "I LOVE YOU TOO PUMPKIN!"
-    elsif say == say.upcase
-        "NO, NOT SINCE 1938!"
+    elsif phrase == phrase.upcase
+    puts "NO, NOT SINCE 1938!"
     else
-      "HUH?! SPEAK UP, SONNY!"
+    puts "HUH?! SPEAK UP, SONNY!"
   end
 end
 
